d010759243a3c6aaa18dd7b8aa1f984485594da7
khizrvd <khizr.farooqui@venturedive.com>
2022-01-07 12:30:48 +0500

    Portfolio Web App in progress

--- lib/about/bloc/about_bloc.dart
+++ lib/about/bloc/about_bloc.dart
@@ -1,13 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:portfolio/about/repository_layer/about_repository_layer.dart';
+import 'package:portfolio/about/repository_layer/models/about_repository_model.dart';
 
 part 'about_event.dart';
 part 'about_state.dart';
 
 class AboutBloc extends Bloc<AboutEvent, AboutState> {
-  AboutBloc() : super(AboutInitial()) {
-    on<AboutEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AboutRepoLayer aboutRepository;
+  AboutBloc(this.aboutRepository) : super(const AboutState()) {
+    on<AboutDataLoaded>(
+      (event, emit) async {
+        emit(
+          state.copyWith(
+            aboutStatus: AboutStatus.loading,
+          ),
+        );
+        final _aboutData = await aboutRepository.getAboutData();
+        emit(
+          state.copyWith(
+            aboutStatus: AboutStatus.loaded,
+            aboutData: _aboutData,
+          ),
+        );
+      },
+    );
   }
 }
